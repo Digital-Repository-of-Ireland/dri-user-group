1dc87a16bc546d8e89a1e8f46c095e7aff79127c
Raymond Noonan <raymond.noonan@nuim.ie>
2012-10-11 15:53:05 +0100

    [App -> Engine] removing files (3)

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-UserGroup::Application.config.secret_token = '5c3ed83091b432212a1f91c7b1f73455e04dad25c74c8c109ef4459a468a3badb8c9035cf04d20b0a0532a699d24c384fa47a1b6724c59a5019ce63172cce174'
