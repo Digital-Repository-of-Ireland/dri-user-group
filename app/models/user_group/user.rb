007c8b12462c14535ef876d01b5e942f4343bacf
Raymond Noonan <raymond.noonan@nuim.ie>
2013-02-27 14:40:39 +0000

    Updated gem to autoload required modules for user

--- app/models/user_group/user.rb
+++ app/models/user_group/user.rb
@@ -1,7 +1,3 @@
-# TODO: Fix http://stackoverflow.com/questions/3356742/best-way-to-load-module-class-from-lib-folder-in-rails-3
-require 'user_group/user_security'
-require 'user_group/user_options'
-
 module UserGroup
     class User < ActiveRecord::Base
         include UserGroup::UserSecurity
--- lib/user_group.rb
+++ lib/user_group.rb
@@ -1,4 +1,6 @@
 require "user_group/engine"
 
 module UserGroup
+    autoload :UserSecurity, 'user_group/user_security'
+    autoload :UserOptions, 'user_group/user_options'
 end
