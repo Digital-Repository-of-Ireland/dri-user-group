b44328b4cdace481b64bd1406cbd3c8dd97f4a4f
Raymond Noonan <raymond.noonan@nuim.ie>
2013-03-13 15:50:13 +0000

    Added get set methods for profile view_level

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -2,3 +2,5 @@ SETTING_DEFAULT_GROUP = 'registered'
 SETTING_ADMIN_GROUP = 'admin'
 SETTING_USER_ORDER = 'second_name'
 SETTING_GROUP_ORDER = 'name'
+PROFILE_VIEW_LEVELS = { 0 => 'private', 1 => 'public', 2 => 'registered' }
+SETTING_PROFILE_INDEX_VIEW_LEVELS = [1 , 2]
--- lib/user_group/user_options.rb
+++ lib/user_group/user_options.rb
@@ -4,12 +4,21 @@ module UserGroup
 
     included do
       before_save :set_locale
-      attr_accessible :locale
+      attr_accessible :locale, :view_level
     end
 
     def set_locale
       self.locale = I18n.locale if self.locale.blank?
     end
 
+    def set_view_level(level)
+      int_level = PROFILE_VIEW_LEVELS.key(level)
+      self.view_level = int_level.nil? ? 0 : int_level
+    end
+
+    def get_view_level
+      level =  PROFILE_VIEW_LEVELS[self.view_level]
+      return level.nil? ? PROFILE_VIEW_LEVELS[0] : level
+    end
   end
 end
