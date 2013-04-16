6f71d7fcfa72b23fe0d6eb60409ae475f7bfdf99
Raymond Noonan <raymond.noonan@nuim.ie>
2013-04-16 16:12:26 +0100

    Same as last commit

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -5,7 +5,7 @@ SETTING_ORDER_GROUP = 'name'
 PROFILE_VIEW_LEVELS = { 0 => 'private', 1 => 'public', 2 => 'registered' }
 SETTING_PROFILE_INDEX_VIEW_LEVELS = [1 , 2]
 #If set to 0 it never expires
-SETTING_TOKEN_EXPIRY_DAYS = 0
+SETTING_PROFILE_TOKEN_EXPIRY_DAYS = 0
 SETTING_PROFILE_GRAVATAR_ID="gravatar"
 SETTING_PROFILE_DEFAULT_PHOTO="default_photo.jpg"
 
