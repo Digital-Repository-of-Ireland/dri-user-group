5ae66b61944d983491d807c929ebea857438ae89
Raymond Noonan <raymond.noonan@nuim.ie>
2013-04-18 15:19:27 +0100

    Fixing bug where validator ran on empty string

--- lib/user_group/user_options.rb
+++ lib/user_group/user_options.rb
@@ -1,7 +1,7 @@
 module UserGroup
   class ImageLinkValidator < ActiveModel::Validator
     def validate(record)
-      unless record.image_link.nil?
+      unless record.image_link.nil? or record.image_link.blank?
         if record.image_link != SETTING_PROFILE_GRAVATAR_ID
           record.errors[:image_link] << I18n.t("user_groups.users.errors.validation_image_link") if FastImage.type(record.image_link).nil?
         end
