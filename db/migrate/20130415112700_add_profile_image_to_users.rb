bb2a2e19d0ce32d39717537ddb491750b1e9a584
Raymond Noonan <raymond.noonan@nuim.ie>
2013-04-18 15:19:05 +0100

    Attempt to set default to nil rather than ''

--- db/migrate/20130415112700_add_profile_image_to_users.rb
+++ db/migrate/20130415112700_add_profile_image_to_users.rb
@@ -1,5 +1,5 @@
 class AddProfileImageToUsers < ActiveRecord::Migration
   def change
-    add_column :user_group_users, :image_link, :string
+    add_column :user_group_users, :image_link, :string, default: nil
   end
 end
