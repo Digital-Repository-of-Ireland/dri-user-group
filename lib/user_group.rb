76dd76aacf7cd6761220c07c9f91f37597e3e5bd
Raymond Noonan <raymond.noonan@nuim.ie>
2013-06-11 17:14:05 +0100

    Added permissions to autoload

--- lib/user_group.rb
+++ lib/user_group.rb
@@ -3,4 +3,6 @@ require "user_group/engine"
 module UserGroup
     autoload :UserSecurity, 'user_group/user_security'
     autoload :UserOptions, 'user_group/user_options'
+    autoload :Permissions, 'user_group/permissions'
+    autoload :PermissionsCheck, 'user_group/permissions_check'
 end
