5fe13be2b42225e6c764b0ddafbffc5ab3793de9
Raymond Noonan <raymond.noonan@nuim.ie>
2013-06-19 16:30:15 +0100

    autoload new files

--- lib/user_group.rb
+++ lib/user_group.rb
@@ -3,6 +3,8 @@ require "user_group/engine"
 module UserGroup
     autoload :UserSecurity, 'user_group/user_security'
     autoload :UserOptions, 'user_group/user_options'
-    autoload :Permissions, 'user_group/permissions'
-    autoload :PermissionsCheck, 'user_group/permissions_check'
+    autoload :PermissionsCheck, 'access_controls/permissions_check'
+    autoload :RightsMetadataDatastreamOverride, 'access_controls/overrides/rights_metadata_datastream_override'
+    autoload :PermissionsSolrDocOverride, 'access_controls/overrides/permissions_solr_doc_override'
+    autoload :RightsMetadataModelMixinOverride, 'access_controls/overrides/rights_metadata_model_mixin_override'
 end
