ea634f5a819c5e94cec3fcd183d1dc690fc604a4
Raymond Noonan <raymond.noonan@nuim.ie>
2013-06-21 11:31:43 +0100

    Autoload shared_ability

--- lib/user_group.rb
+++ lib/user_group.rb
@@ -3,9 +3,10 @@ require "user_group/engine"
 module UserGroup
     autoload :UserSecurity, 'user_group/user_security'
     autoload :UserOptions, 'user_group/user_options'
+    autoload :SharedAbility, 'access_controls/shared_ability'
     autoload :PermissionsCheck, 'access_controls/permissions_check'
     autoload :InheritanceMethods, 'access_controls/inheritance_methods'
-    autoload :RightsMetadataDatastreamOverride, 'access_controls/overrides/rights_metadata_datastream_override'
     autoload :PermissionsSolrDocOverride, 'access_controls/overrides/permissions_solr_doc_override'
+    autoload :RightsMetadataDatastreamOverride, 'access_controls/overrides/rights_metadata_datastream_override'
     autoload :RightsMetadataModelMixinOverride, 'access_controls/overrides/rights_metadata_model_mixin_override'
 end
