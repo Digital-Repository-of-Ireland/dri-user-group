d7a4d894ed02ad2e7d0b2b550c8fd91db4f58ab6
Raymond Noonan <raymond.noonan@nuim.ie>
2013-06-28 12:27:45 +0100

    Autoload SolrAccessControls

--- lib/user_group.rb
+++ lib/user_group.rb
@@ -6,6 +6,7 @@ module UserGroup
     autoload :SharedAbility, 'access_controls/shared_ability'
     autoload :PermissionsCheck, 'access_controls/permissions_check'
     autoload :InheritanceMethods, 'access_controls/inheritance_methods'
+    autoload :SolrAccessControls, 'access_controls/solr_access_controls'
     autoload :PermissionsSolrDocOverride, 'access_controls/overrides/permissions_solr_doc_override'
     autoload :RightsMetadataDatastreamOverride, 'access_controls/overrides/rights_metadata_datastream_override'
     autoload :RightsMetadataModelMixinOverride, 'access_controls/overrides/rights_metadata_model_mixin_override'
