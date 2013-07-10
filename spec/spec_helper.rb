6b9a6543198f75b10a0907ddbed1e1d31469c995
Jimmy Tang <jtang@tchpc.tcd.ie>
2013-07-10 11:58:23 -0300

    enable simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,12 @@
 # loaded once.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'app', 'models'))
+
+require 'simplecov'
+require 'rspec'
+
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
