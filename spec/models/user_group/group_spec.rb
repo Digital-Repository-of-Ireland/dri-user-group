48380e0e4a13a5dcd495e91be5a10718b0b1b7ce
Jimmy Tang <jtang@tchpc.tcd.ie>
2013-07-15 16:38:31 +0100

    comment out tests

--- spec/models/user_group/group_spec.rb
+++ spec/models/user_group/group_spec.rb
@@ -1,8 +1,24 @@
 require 'spec_helper'
 
-describe UserGroup do
+describe UserGroup::Group do
   it "should create a valid group object" do
     @group = UserGroup::Group.new
     @group.should_not be_valid
   end
+
+  it "should be toggleable" do
+    @group = UserGroup::Group.new
+    @group.toggle_lock.should be_true
+    @group.toggle_lock.should_not be_true
+  end
+
+#  it "should test full_memberships" do
+#    @group = UserGroup::Group.new
+#    @group.full_memberships.should be_empty
+#  end
+#
+#  it "should test pending_memberships" do
+#    @group = UserGroup::Group.new
+#    @group.pending_memberships.should be_empty
+#  end
 end
