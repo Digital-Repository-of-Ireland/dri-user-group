2ef27ab669bb87ceadaf8a13c858b5d05310f725
Jimmy Tang <jtang@tchpc.tcd.ie>
2013-07-15 16:11:26 +0100

    test membership model

--- spec/models/user_group/membership_spec.rb
+++ spec/models/user_group/membership_spec.rb
@@ -1,8 +1,27 @@
 require 'spec_helper'
 
 describe UserGroup::Membership do
-  it "should create a valid membership object" do
+  it "should create a membership object" do
     @membership = UserGroup::Membership.new
-    #membership.should_be valid
+    @membership.should_not be_valid
   end
+
+  it "should be approved" do
+    @membership = UserGroup::Membership.new
+    @membership.should_not be_valid
+
+    @membership.approve_membership('notadmin@host.domain')
+    @membership.should_not be_valid
+
+    @membership.approved?.should be_true
+  end
+
+  it "should not be approved" do
+    @membership = UserGroup::Membership.new
+    @membership.should_not be_valid
+
+    @membership.approved?.should_not be_true
+  end
+
 end
+
