c838c0e7559c21ce74f713f6db154498a7729913
Jimmy Tang <jtang@tchpc.tcd.ie>
2013-07-17 21:49:13 +0100

    create user object in test

--- spec/controllers/user_group/users_spec.rb
+++ spec/controllers/user_group/users_spec.rb
@@ -1,7 +1,13 @@
 require 'spec_helper'
 
 describe 'Users' do
+
   before(:each) do
     @routes = UserGroup::Engine.routes
   end
+
+  it "should create a new user object" do
+    @user = UserGroup::UsersController.new
+  end
+
 end
