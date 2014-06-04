5bee1ad40104d31e34a342a11a5ff3589fb375f2
Stuart Kenny <skenny@tchpc.tcd.ie>
2014-06-04 12:32:08 +0100

    Fix error in migration

--- db/migrate/20140526155314_remove_shib_columns_from_users.rb
+++ db/migrate/20140526155314_remove_shib_columns_from_users.rb
@@ -1,7 +1,7 @@
 class RemoveShibColumnsFromUsers < ActiveRecord::Migration
   def up
-    remove_column :user_group_users, :provider, :string
-    remove_column :user_group_users, :uid, :string
+    remove_column :user_group_users, :provider
+    remove_column :user_group_users, :uid
   end
 
   def down
